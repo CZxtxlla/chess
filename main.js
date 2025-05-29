6a7498d83c06101ccb6b35f48abebe44b5796e44
Charles Zitella <cszitella@gmail.com>
2025-05-29 14:20:35 -0400

    added highlight to legal move squares

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 const chess = new Chess();
+var whiteSquareGrey = '#a9a9a9'
+var blackSquareGrey = '#696969'
 
-const board = ChessBoard('board', {
-    position: chess.fen(),
-    draggable: true,
-    onDrop: onDrop,
-});
+function removeGreySquares () {
+    $('#board .square-55d63').css('background', '')
+}
+
+function greySquare (square) {
+var $square = $('#board .square-' + square)
+
+var background = whiteSquareGrey
+if ($square.hasClass('black-3c85d')) {
+    background = blackSquareGrey
+}
+
+$square.css('background', background)
+}
+
+function onDragStart (source, piece) {
+    // do not pick up pieces if the game is over
+    if (chess.game_over()) return false
+  
+    // or if it's not that side's turn
+    if ((chess.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (chess.turn() === 'b' && piece.search(/^w/) !== -1)) {
+      return false
+    }
+}
 
 function onDrop(source, target) {
+    removeGreySquares()
+
     const moves = chess.moves({ verbose: true });
     const moveObj = moves.find(m => m.from === source && m.to === target); // get the move object to check for promotion
 
@@ -28,3 +52,40 @@ function onDrop(source, target) {
     if (move === null) return 'snapback';
     window.setTimeout(makeBestMove, 250);
 }
+
+function onMouseoverSquare (square, piece) {
+    // get list of possible moves for this square
+    var moves = chess.moves({
+      square: square,
+      verbose: true
+    })
+  
+    // exit if there are no moves available for this square
+    if (moves.length === 0) return
+  
+    // highlight the square they moused over
+    greySquare(square)
+  
+    // highlight the possible squares for this piece
+    for (var i = 0; i < moves.length; i++) {
+      greySquare(moves[i].to)
+    }
+}
+
+function onMouseoutSquare (square, piece) {
+    removeGreySquares()
+}
+
+function onSnapEnd () {
+    board.position(chess.fen())
+}
+
+
+const board = ChessBoard('board', {
+    position: chess.fen(),
+    draggable: true,
+    onDrop: onDrop,
+    onMouseoutSquare: onMouseoutSquare,
+    onMouseoverSquare: onMouseoverSquare,
+    onSnapEnd: onSnapEnd,
+});
